bc7250c89c45f96c21e08ed84b16b44908aae58b
autosearch <105331946+charlescornelio@users.noreply.github.com>
2024-10-07 15:43:19 +0700

    feat: create property tenant form

---
+++ apps/api/src/controllers/property-controller.ts
@@ -0,0 +1,72 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "@/prisma";
+
+//get all Pagination
+export async function Pagination(req: Request, res: Response, next: NextFunction) {
+   try {
+      const { page = 1, limit = 3 } = req.query;
+      const offset = (Number(page) - 1) * Number(limit);
+
+      const events = await prisma.property.findMany({
+         skip: offset,
+         take: Number(limit),
+      });
+
+      const totalEvents = await prisma.property.count();
+
+      return res.status(200).json({
+         data: events,
+         meta: {
+            totalEvents,
+            currentPage: Number(page),
+            totalPages: Math.ceil(totalEvents / Number(limit)),
+         },
+      });
+   } catch (error) {
+      next(error);
+   }
+}
+
+//get all property
+
+export async function createProperty(req: Request, res: Response, next: NextFunction) {
+   try {
+      const {
+         name,
+         address,
+         propertydescription,
+         pictureUrl,
+         room,
+         reviews,
+         type,
+         roomDescription,
+         roomCapacity,
+         price,
+         bookings,
+         tenantId,
+      } = req.body;
+
+      await prisma.property.create({
+         data: {
+            name,
+            address,
+            description: propertydescription,
+            pictureUrl,
+            room: {
+               create: {
+                  type,
+                  description: roomDescription,
+                  pictureUrl,
+                  price,
+                  roomCapacity,
+                  bookings,
+               },
+            },
+            reviews,
+            tenantId,
+         },
+      });
+   } catch (error) {
+      next(Error);
+   }
+}
