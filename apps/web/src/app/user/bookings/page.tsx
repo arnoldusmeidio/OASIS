46fb18f2b779fb7b29b04059270f8db53771faa8
anchansp <andhikasp1701jp@gmail.com>
2024-10-21 09:53:07 +0700

    chore: integrated with the latest development branch

--- apps/web/src/app/user/bookings/page.tsx
+++ apps/web/src/app/user/bookings/page.tsx
@@ -1,10 +1,12 @@
 import BookingCard from "@/components/book/BookingCard";
+import Navbar from "@/components/Navbar";
 
 import React from "react";
 
 export default function BookingPage() {
    return (
       <div>
+         <Navbar />
          <BookingCard />
       </div>
    );
