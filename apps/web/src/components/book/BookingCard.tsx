0582ce2c070a3553f8d8498a7f81490a42206b3c
anchansp <andhikasp1701jp@gmail.com>
2024-10-21 09:39:54 +0700

    chore: (user get booking) same as before

--- apps/web/src/components/book/BookingCard.tsx
+++ apps/web/src/components/book/BookingCard.tsx
@@ -3,14 +3,42 @@
 import React, { useEffect, useState } from "react";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+   AlertDialog,
+   AlertDialogAction,
+   AlertDialogCancel,
+   AlertDialogContent,
+   AlertDialogDescription,
+   AlertDialogFooter,
+   AlertDialogHeader,
+   AlertDialogTitle,
+   AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuLabel,
+   DropdownMenuRadioGroup,
+   DropdownMenuRadioItem,
+   DropdownMenuSeparator,
+   DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
+import { ToastAction } from "@/components/ui/toast";
 import { format } from "date-fns";
 import { Booking } from "@/types/booking";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Skeleton } from "../ui/skeleton";
 
 export default function BookingCard() {
+   const router = useRouter();
+
+   const [isLoading, setIsLoading] = useState(true);
    const [bookingData, setBookingData] = useState({ data: [] });
+   const [dataReplica, setDataReplica] = useState({ data: [] });
    const eventGetter = async () => {
       try {
          const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/v1/bookings`, {
@@ -20,7 +48,11 @@ export default function BookingCard() {
          });
 
          const resData = await res.json();
-         setBookingData(resData);
+         if (resData.ok) {
+            setBookingData(resData);
+            setDataReplica(resData);
+         }
+         setIsLoading(false);
       } catch (error) {
          console.error(error);
       }
@@ -28,6 +60,8 @@ export default function BookingCard() {
    useEffect(() => {
       eventGetter();
    }, []);
+
+   if (isLoading) return <Skeleton className="flex flex-col gap-4 px-4 py-4" />;
 
    if (bookingData.data.length == 0) {
       return (
@@ -50,59 +84,117 @@ export default function BookingCard() {
    }
 
    return (
-      <div className="flex flex-col gap-4 px-4 py-4">
-         <Accordion type="single" collapsible>
-            {(bookingData as any)?.data?.map((e: Booking, index: number) => (
-               <AccordionItem value={e.id} key={e.id}>
-                  <AccordionTrigger>
-                     Booking Number: <h6 className="font-bold">{e.bookingNumber}</h6>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                     <Card className="w-full shadow-md">
-                        <CardHeader>
-                           <CardTitle>Booking Info</CardTitle>
-                           <CardDescription>
-                              {!e.endDate
-                                 ? `${format(e.startDate, "LLL dd, y")}`
-                                 : `${format(e.startDate, "LLL dd, y")} - ${format(e.endDate, "LLL dd, y")}`}
-                              <Image
-                                 src={e.room.pictureUrl}
-                                 alt=""
-                                 width={200}
-                                 height={200}
-                                 className="flex rounded-lg"
-                              ></Image>
-                           </CardDescription>
-                        </CardHeader>
-                        <CardContent className="grid gap-4">
-                           <div className="flex items-center space-x-4 rounded-md border p-4">
-                              <div className="flex-1 space-y-1">
-                                 <p className="text-sm font-medium leading-none">Payment Status:</p>
-                                 <p className="text-muted-foreground text-sm">{e.paymentStatus}</p>
-                              </div>
+      <main className="flex w-full justify-center">
+         <div className="flex w-full min-w-[375px] max-w-7xl flex-col gap-8 px-8 py-4">
+            <div className="flex justify-end gap-4 px-4 py-4">
+               <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                     <Button variant="outline">Sort</Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56">
+                     <DropdownMenuLabel>Sort by:</DropdownMenuLabel>
+                     <DropdownMenuSeparator />
+                     <DropdownMenuRadioGroup>
+                        <DropdownMenuRadioItem value="date_asc">Date (Asc)</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="date_desc">Date (Desc)</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="booking_asc">Booking Number (Asc)</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="booking_desc">Booking Number (Desc)</DropdownMenuRadioItem>
+                     </DropdownMenuRadioGroup>
+                  </DropdownMenuContent>
+               </DropdownMenu>
+            </div>
+            <Accordion type="single" collapsible>
+               {(bookingData as any)?.data?.map((e: Booking, index: number) => (
+                  <AccordionItem value={e.id} key={e.id}>
+                     <AccordionTrigger>
+                        <div className="grid grid-cols-1 sm:grid-cols-2">
+                           <div>Booking Number:</div>
+                           <div>
+                              <h6 className="font-bold"> {e.bookingNumber}</h6>
                            </div>
-                        </CardContent>
-                        <CardFooter className="flex gap-4">
-                           {e.paymentStatus == "PENDING" ? (
-                              <Link href="/">
-                                 <Button className="w-full">Pay Now</Button>
-                              </Link>
-                           ) : (
-                              ""
-                           )}
-                           {e.paymentStatus == "PENDING" ? (
-                              <Link href="/">
-                                 <Button className="w-full">Cancel Booking</Button>
-                              </Link>
-                           ) : (
-                              ""
-                           )}
-                        </CardFooter>
-                     </Card>
-                  </AccordionContent>
-               </AccordionItem>
-            ))}
-         </Accordion>
-      </div>
+                        </div>
+                     </AccordionTrigger>
+                     <AccordionContent>
+                        <Card className="w-full shadow-md">
+                           <CardHeader>
+                              <CardTitle>Booking Info</CardTitle>
+                              <CardDescription>
+                                 {!e.endDate
+                                    ? `${format(e.startDate, "LLL dd, y")}`
+                                    : `${format(e.startDate, "LLL dd, y")} - ${format(e.endDate, "LLL dd, y")}`}
+                                 <Image
+                                    src={e.room.pictureUrl}
+                                    alt=""
+                                    width={200}
+                                    height={200}
+                                    className="flex rounded-lg"
+                                 ></Image>
+                              </CardDescription>
+                           </CardHeader>
+                           <CardContent className="grid gap-4">
+                              <div className="flex items-center space-x-4 rounded-md border p-4">
+                                 <div className="flex-1 space-y-1">
+                                    <p className="text-sm font-medium leading-none">Payment Status:</p>
+                                    <p className="text-muted-foreground text-sm">{e.paymentStatus}</p>
+                                 </div>
+                              </div>
+                           </CardContent>
+                           <CardFooter className="flex gap-4">
+                              {e.paymentStatus == "PENDING" ? (
+                                 <Link href="/">
+                                    <Button className="w-full">Pay Now</Button>
+                                 </Link>
+                              ) : (
+                                 ""
+                              )}
+                              {e.paymentStatus == "PENDING" ? (
+                                 <AlertDialog>
+                                    <AlertDialogTrigger asChild>
+                                       <Button variant="outline">Cancel Booking</Button>
+                                    </AlertDialogTrigger>
+                                    <AlertDialogContent>
+                                       <AlertDialogHeader>
+                                          <AlertDialogTitle>Cancel your booking?</AlertDialogTitle>
+                                          <AlertDialogDescription>
+                                             By confirming cancellation, all associated reservations will be released.
+                                             Please review our policies regarding cancellations carefully.
+                                          </AlertDialogDescription>
+                                       </AlertDialogHeader>
+                                       <AlertDialogFooter>
+                                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                          <AlertDialogAction
+                                             onClick={async () => {
+                                                try {
+                                                   const res = await fetch(
+                                                      `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/v1/bookings/${e.bookingNumber}`,
+                                                      {
+                                                         method: "DELETE",
+                                                         headers: { "Content-Type": "application/json" },
+                                                         credentials: "include",
+                                                      },
+                                                   );
+                                                   toast.success("Booking Successfully Cancelled", { duration: 1500 });
+                                                   eventGetter();
+                                                } catch (error) {
+                                                   console.error(error);
+                                                }
+                                             }}
+                                          >
+                                             Continue
+                                          </AlertDialogAction>
+                                       </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                 </AlertDialog>
+                              ) : (
+                                 ""
+                              )}
+                           </CardFooter>
+                        </Card>
+                     </AccordionContent>
+                  </AccordionItem>
+               ))}
+            </Accordion>
+         </div>
+      </main>
    );
 }
