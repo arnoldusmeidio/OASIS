18f02720d8673fad568123302525182a84c64064
arnoldusmeidio <arnoldusmeidio@gmail.com>
2024-10-24 13:05:54 +0700

    Feature/currency os 33 (#16)
    * feat: currency changing based on preferred setting
    
    * feat: currency changing based on preferred setting

--- apps/web/src/app/select-role/page.tsx
+++ apps/web/src/app/select-role/page.tsx
@@ -37,7 +37,6 @@ export default function SelectRolePage() {
             credentials: "include",
          });
          const data = await response.json();
-         console.log(data);
 
          if (!data.ok) {
             toast.error(data.message);
