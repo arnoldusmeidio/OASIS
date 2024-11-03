1df24521d2d641e5a1daeccabc43e3dab5f66f0e
arnoldusmeidio <arnoldusmeidio@gmail.com>
2024-11-03 23:33:17 +0700

    build: test deployment

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,15 @@ module.exports = {
    apps: [
       {
          namespace: "Oasis Project",
+         name: "web",
+         script: "turbo serve",
+         cwd: "./apps/web",
+         watch: ".",
+      },
+      {
+         namespace: "Oasis Project",
          name: "api",
-         script: "npm run serve",
+         script: "turbo serve",
          cwd: "./apps/api",
          watch: ".",
       },
