e0a058b785affa4e025fa76652abe45a76040a39
arnoldusmeidio <arnoldusmeidio@gmail.com>
2024-11-03 18:47:41 +0700

    build: add pm2 ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+   apps: [
+      {
+         namespace: "Oasis Project",
+         name: "api",
+         script: "npm run serve",
+         cwd: "./apps/api",
+         watch: ".",
+      },
+   ],
+};
